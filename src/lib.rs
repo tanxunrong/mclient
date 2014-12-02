747160b932b0ae0618dac608a1c1632e546d21c7
tanxr <tanxrong@gmail.com>
2014-12-02 11:30:19 +0800

    add set & response

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,15 @@ impl FromError<ClientError> for Failure {
 
 pub type McResult<T> = Result<T,Failure>;
 
+#[deriving(Show)]
+pub enum Response {
+    Stored,
+    NotStored,
+    InvalidCmd,
+    ClientErr(String),
+    ServerErr(String)
+}
+
 impl Client {
 
     pub fn new(addr:&str) -> McResult<Client> {
@@ -54,9 +63,24 @@ impl Client {
                 Ok(Client{addr:addr,conn:RefCell::new(conn)})
             },
             None => {
-                Err(Failure::Client(ClientError{desc:"invalid addr".into_string()}))
+                Err(FromError::from_error(ClientError{desc:"invalid addr".into_string()}))
             }
         }
+    }
+
+    pub fn set(&self,key:&str,flag:u16,expire:uint,data:&str) -> McResult<Response> {
+        let cmd = format_args!(std::fmt::format,"set {} {} {} {}\r\n{}\r\n",key,flag,expire,data.as_slice().as_bytes().len(),data);
+        println!("cmd {}",cmd);
+        try!(self.conn.borrow_mut().write(cmd.as_slice().as_bytes()));
+        let ret = try!(self.conn.borrow_mut().read_to_end()).into_ascii().into_string();
+        if ret.starts_with("STORED\r\n") { return Ok(Response::Stored); }
+        else if ret.starts_with("NOT_STORED\r\n") { return Ok(Response::NotStored); }
+        else if ret.starts_with("ERROR\r\n" ) { return Ok(Response::InvalidCmd); }
+        else if ret.starts_with("CLIENT_ERROR") { let err = ret.slice(12,ret.len()-4).into_string();
+            return Ok(Response::ClientErr(err));}
+        else if ret.starts_with("SERVER_ERROR") {  let err = ret.slice(12,ret.len()-4).into_string();
+            return Ok(Response::ServerErr(err));}
+        else { return Ok(Response::InvalidCmd); }
     }
 
 }
@@ -65,5 +89,10 @@ impl Client {
 fn test_new_mc() {
     let c = Client::new("127.0.0.1:11211");
     assert!(c.is_ok());
+    let res = c.unwrap().set("go",0u16,0u,"for");
+    match res {
+        Ok(r) => { println!("res {}",r); },
+        Err(e) => { panic!(e); }
+    }
 }
 
