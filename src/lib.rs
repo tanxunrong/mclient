2a1ff30d94788ce382188966bce91cdbc982d350
tanxr <tanxrong@gmail.com>
2014-12-02 14:32:47 +0800

    add parser

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,92 @@ pub enum Response {
     ServerErr(String)
 }
 
+pub struct Parser<T> {
+    reader : T
+}
+
+impl <T> Parser<T> where T:Reader {
+    
+    pub fn new(reader: T) -> Parser<T> {
+        Parser { reader: reader }
+    }
+
+    #[inline]
+    fn expect_char(&mut self, refchar: char) -> McResult<()> {
+        if try!(self.reader.read_byte()) as char == refchar {
+            Ok(())
+        } else {
+            Err(FromError::from_error(ClientError{desc:"Invalid byte in response".into_string()}))
+        }
+    }
+
+    #[inline]
+    fn expect_newline(&mut self) -> McResult<()> {
+        match try!(self.reader.read_byte()) as char {
+            '\n' => Ok(()),
+            '\r' => self.expect_char('\n'),
+            _ => Err(FromError::from_error(ClientError{desc:"Expect new line,Invalid byte in response".into_string()}))
+        }
+    }
+
+    fn read_line(&mut self) -> McResult<Vec<u8>> {
+        let mut rv = vec![];
+
+        loop {
+            let b = try!(self.reader.read_byte());
+            match b as char {
+                '\n' => { break; }
+                '\r' => {
+                    try!(self.expect_char('\n'));
+                    break;
+                },
+                _ => { rv.push(b) }
+            };
+        }
+
+        Ok(rv)
+    }
+
+    fn read_string_line(&mut self) -> McResult<String> {
+        match String::from_utf8(try!(self.read_line())) {
+            Err(_) => {
+                Err(FromError::from_error(ClientError{desc:"Expect string,Invalid byte in response".into_string()}))
+            }
+            Ok(value) => Ok(value)
+        }
+    }
+
+    pub fn parse_value(&mut self) -> McResult<Response> {
+        let ret = self.read_string_line().unwrap();
+        let line = ret.as_slice();
+        if line.len() < 5 {
+                return Err(FromError::from_error(ClientError{desc:"Expect more,Invalid byte in response".into_string()}));
+        }
+        if line.starts_with("STORED") {
+            Ok(Response::Stored)
+        } 
+        else if line.starts_with("NOT_STORED") {
+            Ok(Response::NotStored)
+        }
+        else if line.starts_with("ERROR") {
+            Ok(Response::InvalidCmd)
+        }
+        else if line.starts_with("CLIENT_ERROR") {
+            let err = line.slice(12,line.len()-4).into_string();
+            Ok(Response::ClientErr(err))
+        }
+        else if line.starts_with("SERVER_ERROR") {
+            let err = line.slice(12,line.len()-4).into_string();
+            Ok(Response::ServerErr(err))
+        }
+        else {
+             Err(FromError::from_error(ClientError{desc:"invalid response".into_string()}))
+        }
+
+    }
+
+}
+
 impl Client {
 
     pub fn new(addr:&str) -> McResult<Client> {
@@ -67,26 +153,16 @@ impl Client {
         }
     }
 
-    pub fn set(&self,key:&str,flag:u16,expire:uint,data:&str) -> McResult<Response> {
+    pub fn set(&mut self,key:&str,flag:u16,expire:uint,data:&str) -> McResult<Response> {
         let cmd = format_args!(std::fmt::format,"set {} {} {} {}\r\n{}\r\n",key,flag,expire,data.as_slice().as_bytes().len(),data);
-        println!("{}",cmd);
 
         let mut conn = self.conn.borrow_mut();
-        conn.set_timeout(Some(5000u64));
+        conn.set_timeout(Some(1000u64));
 
         try!(conn.write(cmd.as_slice().as_bytes()));
-
-        let ret = try!(conn.read_to_string());
-
-        if ret.starts_with("STORED\r\n") { return Ok(Response::Stored); }
-        else if ret.starts_with("NOT_STORED\r\n") { return Ok(Response::NotStored); }
-        else if ret.starts_with("ERROR\r\n" ) { return Ok(Response::InvalidCmd); }
-        else if ret.starts_with("CLIENT_ERROR") { let err = ret.slice(12,ret.len()-4).into_string();
-            return Ok(Response::ClientErr(err));}
-        else if ret.starts_with("SERVER_ERROR") {  let err = ret.slice(12,ret.len()-4).into_string();
-            return Ok(Response::ServerErr(err));}
-        else { return Ok(Response::InvalidCmd); }
-    }
-
+        let mut parser = Parser::new(&mut conn as &mut Reader);
+        let res = try!(parser.parse_value());
+        Ok(res)
+   }
 }
 
